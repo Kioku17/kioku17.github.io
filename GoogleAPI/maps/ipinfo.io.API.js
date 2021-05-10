9fc7cce0060cc2e2c43e3cb068d3527c4b0f6680
Kioku17 <63692852+Kioku17@users.noreply.github.com>
2021-05-10 20:06:09 +0700

    Update ipinfo.io.API.js

--- GoogleAPI/maps/ipinfo.io.API.js
+++ GoogleAPI/maps/ipinfo.io.API.js
@@ -4,7 +4,22 @@ function getLocation() {
  
   function onLocationGot(info) {
     let position = info.loc.split(",");
-    $("#lat").text(position[0]);
-    $("#lon").text(position[1]);
+    var lat = position[0];
+    var lon = position[1];
+          //Khoi tao Map
+          function initialize() {
+            //Khai bao cac thuoc tinh
+            var mapProp = {
+              //Tam ban do, quy dinh boi kinh do va vi do
+              center:new google.maps.LatLng(51.508742, -0.120850),
+              //set default zoom cua ban do khi duoc load
+              zoom:5,
+              //Dinh nghia type
+              mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+            //Truyen tham so cho cac thuoc tinh Map cho the div chua Map
+            var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+          }
+          google.maps.event.addDomListener(window, 'load', initialize);
   }
 });
